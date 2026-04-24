d24703ce2566427f2081818f4c5e08c57747bf54
Alef Hugo <alef65507@gmail.com>
2026-04-24 17:40:35 -0300

    Delete site-adega directory

--- site-adega/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// Configuração centralizada do Axios
-const api = axios.create({
-  // Define a URL base buscando do .env ou usando o padrão do Spring Boot
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080'
-});
-
-export default api;
